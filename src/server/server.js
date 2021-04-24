b09c37e3651f0b5ff572c7e82dee18b1b49fa52d
Sander Ulset <sanderulset1@gmail.com>
2021-04-24 16:17:55 +0200

    Severer (ferskt) nå static filer

--- src/server/server.js
+++ src/server/server.js
@@ -7,7 +7,17 @@ const app = express();
 //Setup app
 app.use(cors());
 app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.resolve(__dirname, "..", "..", "dist")));
 app.use(express.json());
+
+//Since im not using the server part of parcel(just the bundling) i need to be able to serve the files.
+app.use((req, res, next)=>{
+    if(req.method !== "GET" || req.path.startsWith("/api")){
+        return next();
+    }
+
+    return res.sendFile(path.resolve(__dirname, "..", "..", "dist", "index.html"))
+})
 
 app.get("/test", (req, res)=>{
     res.status(200).send("test")
