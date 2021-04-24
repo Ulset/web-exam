47f16f951e2832a291904029e2b6aefb7c943357
Sander Ulset <sanderulset1@gmail.com>
2021-04-24 15:37:05 +0200

    Startet på eksamen basics

---
+++ src/server/server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+
+const app = express();
+
+//Setup app
+app.use(cors());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.get("/test", (req, res)=>{
+    res.status(200).send("test")
+})
+
+
+const port = 8000
+app.listen(port, ()=>{
+    console.log("El server estoy livoooooo")
+})
